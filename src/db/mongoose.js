120ac560936d6643dd5867d42f8d6b0d03493cdd
unknown <cavihai@gmail.com>
2021-03-17 20:54:22 +0200

    new

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose')
 
-/* mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology: true
-}) */
-
-mongoose.connect("mongodb+srv://taskApp:avihaic524310@cluster0.kfuwr.mongodb.net/task-manager-api?retryWrites=true",{
+mongoose.connect('mongodb+srv://taskApp:avihaic524310@cluster0.kfuwr.mongodb.net/task-manager-api?retryWrites=true',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
 })
+
 
 
